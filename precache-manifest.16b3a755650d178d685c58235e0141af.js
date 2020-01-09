40763513645ab72215d7890fb360f176d073ecbf
rakins@gmail.com <ryan.akins@wilksbrothers.com>
2020-01-09 14:05:47 -0600

    Updates

---
+++ precache-manifest.16b3a755650d178d685c58235e0141af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70a0a9dbd34ee204bb8170dc6e569ab0",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "a7b1318344d4bc48bd51",
+    "url": "/monsters/static/css/main.600066ad.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a7b1318344d4bc48bd51",
+    "url": "/monsters/static/js/main.2be60490.chunk.js"
+  },
+  {
+    "revision": "475d84c361081acc4a17",
+    "url": "/monsters/static/js/runtime-main.05e5ea3c.js"
+  }
+]);
